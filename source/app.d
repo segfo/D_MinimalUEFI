1da2905cb5bd841dadf9e64469863483a4c823e4
segfo <k.segfo@gmail.com>
2017-03-25 00:59:09 +0900

    凄い力技で構造体を使うD言語くん
    力がほしいか！ならばやろう。

--- source/app.d
+++ source/app.d
@@ -1,13 +1,42 @@
-import std.stdio;
-
 pragma(startaddress,efi_main);
 
-extern(C) int efi_main()@nogc nothrow{
+extern(C) __gshared void* _D10TypeInfo_i6__initZ;
+extern(C) __gshared void* _D15TypeInfo_Struct6__vtblZ;
+extern(C) __gshared void* _D10TypeInfo_l6__initZ;
+extern(C) __gshared void* _D14TypeInfo_Const6__vtblZ;
+extern(C) __gshared void* _D12TypeInfo_Aya6__initZ;
+// funcに渡されるメモリアドレス
+// （スタックで確保していればスタックのもの、ヒープならヒープのものが渡される）
+// がnullのときに呼ばれる。今回、「Struct」はスタックに確保しているので
+// スタックのアドレスが渡されて、それは常にnullになりえないのでぶっちゃけ中身はなんでもいい。
+extern(C) void _d_assert_msg(){
+	sys_halt();
+}
+
+struct Struct{
+	int a,b;
+	string s;
+	void func(string s){
+		a=0;
+		this.s=s;
+		return;
+	}
+}
+
+void sys_halt(){
 	while(true){
 		asm{
 			cli;
 			hlt;
 		}
 	}
+}
+
+extern(C) int efi_main(){
+	scope(exit){sys_halt();}
+	Struct s={0};
+	s.a=0xdeadbeef;
+	s.b=0xcafecafe;
+	s.func("hello world");
 	return 0;
 }
